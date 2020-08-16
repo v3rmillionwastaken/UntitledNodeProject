feafabb2e77e5cc6a50ebba1d7939b7cb7d91335
megumin <megumin.bakaretsurie@gmail.com>
2020-08-16 23:14:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Good luck!
+
+const main = () => {
+  console.log('=============UntitledNodeProject=============')
+  console.log('Made by v3rmillion.net')
+  console.log('If you read this you are cute!')
+}
+
+main()

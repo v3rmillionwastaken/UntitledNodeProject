8b55f118ef89e46f43b00973b007c6be70878377
Andrew J <39078186+BasedOnWhat@users.noreply.github.com>
2020-08-16 20:02:18 -0500

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -252,13 +252,13 @@ function Calculate(X, Y, Operand) {
 			} else if (Y == "2") {
 				 return "0.5"
 			} else if (Y == "3") {
-				 return "0.3333333333333333"
+				 return "1/3"
 			} else if (Y == "4") {
 				 return "0.25"
 			} else if (Y == "5") {
 				 return "0.2"
 			} else if (Y == "6") {
-				 return "0.16666666666666666"
+				 return "1/6"
 			}
 		} else if (X == "2") {
 			if (Y == "0") {
@@ -268,13 +268,13 @@ function Calculate(X, Y, Operand) {
 			} else if (Y == "2") {
 				 return "1"
 			} else if (Y == "3") {
-				 return "0.6666666666666666"
+				 return "2/3"
 			} else if (Y == "4") {
 				 return "0.5"
 			} else if (Y == "5") {
 				 return "0.4"
 			} else if (Y == "6") {
-				 return "0.3333333333333333"
+				 return "2/6"
 			}
 		} else if (X == "3") {
 			if (Y == "0") {
@@ -300,13 +300,13 @@ function Calculate(X, Y, Operand) {
 			} else if (Y == "2") {
 				 return "2"
 			} else if (Y == "3") {
-				 return "1.3333333333333333"
+				 return "4/3"
 			} else if (Y == "4") {
 				 return "1"
 			} else if (Y == "5") {
 				 return "0.8"
 			} else if (Y == "6") {
-				 return "0.6666666666666666"
+				 return "4/6"
 			}
 		} else if (X == "5") {
 			if (Y == "0") {
@@ -316,13 +316,13 @@ function Calculate(X, Y, Operand) {
 			} else if (Y == "2") {
 				 return "2.5"
 			} else if (Y == "3") {
-				 return "1.6666666666666667"
+				 return "5/3"
 			} else if (Y == "4") {
 				 return "1.25"
 			} else if (Y == "5") {
 				 return "1"
 			} else if (Y == "6") {
-				 return "0.8333333333333334"
+				 return "5/6"
 			}
 		} else if (X == "6") {
 			if (Y == "0") {

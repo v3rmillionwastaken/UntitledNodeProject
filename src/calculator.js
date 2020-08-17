6cede04b4a554b462714f7def0bbe60b5e6366f7
Andrew J <39078186+BasedOnWhat@users.noreply.github.com>
2020-08-16 19:53:28 -0500

    Create calculator.js

---
+++ src/calculator.js
@@ -0,0 +1,463 @@
+function Calculate(X, Y, Operand) {
+	if (Operand == "add") {
+		if (X == "0") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "1"
+			} else if (Y == "2") {
+				 return "2"
+			} else if (Y == "3") {
+				 return "3"
+			} else if (Y == "4") {
+				 return "4"
+			} else if (Y == "5") {
+				 return "5"
+			} else if (Y == "6") {
+				 return "6"
+			}
+		} else if (X == "1") {
+			if (Y == "0") {
+				 return "1"
+			} else if (Y == "1") {
+				 return "2"
+			} else if (Y == "2") {
+				 return "3"
+			} else if (Y == "3") {
+				 return "4"
+			} else if (Y == "4") {
+				 return "5"
+			} else if (Y == "5") {
+				 return "6"
+			} else if (Y == "6") {
+				 return "7"
+			}
+		} else if (X == "2") {
+			if (Y == "0") {
+				 return "2"
+			} else if (Y == "1") {
+				 return "3"
+			} else if (Y == "2") {
+				 return "4"
+			} else if (Y == "3") {
+				 return "5"
+			} else if (Y == "4") {
+				 return "6"
+			} else if (Y == "5") {
+				 return "7"
+			} else if (Y == "6") {
+				 return "8"
+			}
+		} else if (X == "3") {
+			if (Y == "0") {
+				 return "3"
+			} else if (Y == "1") {
+				 return "4"
+			} else if (Y == "2") {
+				 return "5"
+			} else if (Y == "3") {
+				 return "6"
+			} else if (Y == "4") {
+				 return "7"
+			} else if (Y == "5") {
+				 return "8"
+			} else if (Y == "6") {
+				 return "9"
+			}
+		} else if (X == "4") {
+			if (Y == "0") {
+				 return "4"
+			} else if (Y == "1") {
+				 return "5"
+			} else if (Y == "2") {
+				 return "6"
+			} else if (Y == "3") {
+				 return "7"
+			} else if (Y == "4") {
+				 return "8"
+			} else if (Y == "5") {
+				 return "9"
+			} else if (Y == "6") {
+				 return "10"
+			}
+		} else if (X == "5") {
+			if (Y == "0") {
+				 return "5"
+			} else if (Y == "1") {
+				 return "6"
+			} else if (Y == "2") {
+				 return "7"
+			} else if (Y == "3") {
+				 return "8"
+			} else if (Y == "4") {
+				 return "9"
+			} else if (Y == "5") {
+				 return "10"
+			} else if (Y == "6") {
+				 return "11"
+			}
+		} else if (X == "6") {
+			if (Y == "0") {
+				 return "6"
+			} else if (Y == "1") {
+				 return "7"
+			} else if (Y == "2") {
+				 return "8"
+			} else if (Y == "3") {
+				 return "9"
+			} else if (Y == "4") {
+				 return "10"
+			} else if (Y == "5") {
+				 return "11"
+			} else if (Y == "6") {
+				 return "12"
+			}
+		}
+	} else if (Operand == "subtract") {
+		if (X == "0") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "-1"
+			} else if (Y == "2") {
+				 return "-2"
+			} else if (Y == "3") {
+				 return "-3"
+			} else if (Y == "4") {
+				 return "-4"
+			} else if (Y == "5") {
+				 return "-5"
+			} else if (Y == "6") {
+				 return "-6"
+			}
+		} else if (X == "1") {
+			if (Y == "0") {
+				 return "1"
+			} else if (Y == "1") {
+				 return "0"
+			} else if (Y == "2") {
+				 return "-1"
+			} else if (Y == "3") {
+				 return "-2"
+			} else if (Y == "4") {
+				 return "-3"
+			} else if (Y == "5") {
+				 return "-4"
+			} else if (Y == "6") {
+				 return "-5"
+			}
+		} else if (X == "2") {
+			if (Y == "0") {
+				 return "2"
+			} else if (Y == "1") {
+				 return "1"
+			} else if (Y == "2") {
+				 return "0"
+			} else if (Y == "3") {
+				 return "-1"
+			} else if (Y == "4") {
+				 return "-2"
+			} else if (Y == "5") {
+				 return "-3"
+			} else if (Y == "6") {
+				 return "-4"
+			}
+		} else if (X == "3") {
+			if (Y == "0") {
+				 return "3"
+			} else if (Y == "1") {
+				 return "2"
+			} else if (Y == "2") {
+				 return "1"
+			} else if (Y == "3") {
+				 return "0"
+			} else if (Y == "4") {
+				 return "-1"
+			} else if (Y == "5") {
+				 return "-2"
+			} else if (Y == "6") {
+				 return "-3"
+			}
+		} else if (X == "4") {
+			if (Y == "0") {
+				 return "4"
+			} else if (Y == "1") {
+				 return "3"
+			} else if (Y == "2") {
+				 return "2"
+			} else if (Y == "3") {
+				 return "1"
+			} else if (Y == "4") {
+				 return "0"
+			} else if (Y == "5") {
+				 return "-1"
+			} else if (Y == "6") {
+				 return "-2"
+			}
+		} else if (X == "5") {
+			if (Y == "0") {
+				 return "5"
+			} else if (Y == "1") {
+				 return "4"
+			} else if (Y == "2") {
+				 return "3"
+			} else if (Y == "3") {
+				 return "2"
+			} else if (Y == "4") {
+				 return "1"
+			} else if (Y == "5") {
+				 return "0"
+			} else if (Y == "6") {
+				 return "-1"
+			}
+		} else if (X == "6") {
+			if (Y == "0") {
+				 return "6"
+			} else if (Y == "1") {
+				 return "5"
+			} else if (Y == "2") {
+				 return "4"
+			} else if (Y == "3") {
+				 return "3"
+			} else if (Y == "4") {
+				 return "2"
+			} else if (Y == "5") {
+				 return "1"
+			} else if (Y == "6") {
+				 return "0"
+			}
+		}
+	} else if (Operand == "divide") {
+		if (X == "0") {
+			if (Y == "0") {
+				 return "undefined"
+			} else if (Y == "1") {
+				 return "0"
+			} else if (Y == "2") {
+				 return "0"
+			} else if (Y == "3") {
+				 return "0"
+			} else if (Y == "4") {
+				 return "0"
+			} else if (Y == "5") {
+				 return "0"
+			} else if (Y == "6") {
+				 return "0"
+			}
+		} else if (X == "1") {
+			if (Y == "0") {
+				 return "undefined"
+			} else if (Y == "1") {
+				 return "1"
+			} else if (Y == "2") {
+				 return "0.5"
+			} else if (Y == "3") {
+				 return "0.3333333333333333"
+			} else if (Y == "4") {
+				 return "0.25"
+			} else if (Y == "5") {
+				 return "0.2"
+			} else if (Y == "6") {
+				 return "0.16666666666666666"
+			}
+		} else if (X == "2") {
+			if (Y == "0") {
+				 return "undefined"
+			} else if (Y == "1") {
+				 return "2"
+			} else if (Y == "2") {
+				 return "1"
+			} else if (Y == "3") {
+				 return "0.6666666666666666"
+			} else if (Y == "4") {
+				 return "0.5"
+			} else if (Y == "5") {
+				 return "0.4"
+			} else if (Y == "6") {
+				 return "0.3333333333333333"
+			}
+		} else if (X == "3") {
+			if (Y == "0") {
+				 return "undefined"
+			} else if (Y == "1") {
+				 return "3"
+			} else if (Y == "2") {
+				 return "1.5"
+			} else if (Y == "3") {
+				 return "1"
+			} else if (Y == "4") {
+				 return "0.75"
+			} else if (Y == "5") {
+				 return "0.6"
+			} else if (Y == "6") {
+				 return "0.5"
+			}
+		} else if (X == "4") {
+			if (Y == "0") {
+				 return "undefined"
+			} else if (Y == "1") {
+				 return "4"
+			} else if (Y == "2") {
+				 return "2"
+			} else if (Y == "3") {
+				 return "1.3333333333333333"
+			} else if (Y == "4") {
+				 return "1"
+			} else if (Y == "5") {
+				 return "0.8"
+			} else if (Y == "6") {
+				 return "0.6666666666666666"
+			}
+		} else if (X == "5") {
+			if (Y == "0") {
+				 return "undefined"
+			} else if (Y == "1") {
+				 return "5"
+			} else if (Y == "2") {
+				 return "2.5"
+			} else if (Y == "3") {
+				 return "1.6666666666666667"
+			} else if (Y == "4") {
+				 return "1.25"
+			} else if (Y == "5") {
+				 return "1"
+			} else if (Y == "6") {
+				 return "0.8333333333333334"
+			}
+		} else if (X == "6") {
+			if (Y == "0") {
+				 return "undefined"
+			} else if (Y == "1") {
+				 return "6"
+			} else if (Y == "2") {
+				 return "3"
+			} else if (Y == "3") {
+				 return "2"
+			} else if (Y == "4") {
+				 return "1.5"
+			} else if (Y == "5") {
+				 return "1.2"
+			} else if (Y == "6") {
+				 return "1"
+			}
+		}
+	} else if (Operand == "multiply") {
+		if (X == "0") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "0"
+			} else if (Y == "2") {
+				 return "0"
+			} else if (Y == "3") {
+				 return "0"
+			} else if (Y == "4") {
+				 return "0"
+			} else if (Y == "5") {
+				 return "0"
+			} else if (Y == "6") {
+				 return "0"
+			}
+		} else if (X == "1") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "1"
+			} else if (Y == "2") {
+				 return "2"
+			} else if (Y == "3") {
+				 return "3"
+			} else if (Y == "4") {
+				 return "4"
+			} else if (Y == "5") {
+				 return "5"
+			} else if (Y == "6") {
+				 return "6"
+			}
+		} else if (X == "2") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "2"
+			} else if (Y == "2") {
+				 return "4"
+			} else if (Y == "3") {
+				 return "6"
+			} else if (Y == "4") {
+				 return "8"
+			} else if (Y == "5") {
+				 return "10"
+			} else if (Y == "6") {
+				 return "12"
+			}
+		} else if (X == "3") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "3"
+			} else if (Y == "2") {
+				 return "6"
+			} else if (Y == "3") {
+				 return "9"
+			} else if (Y == "4") {
+				 return "12"
+			} else if (Y == "5") {
+				 return "15"
+			} else if (Y == "6") {
+				 return "18"
+			}
+		} else if (X == "4") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "4"
+			} else if (Y == "2") {
+				 return "8"
+			} else if (Y == "3") {
+				 return "12"
+			} else if (Y == "4") {
+				 return "16"
+			} else if (Y == "5") {
+				 return "20"
+			} else if (Y == "6") {
+				 return "24"
+			}
+		} else if (X == "5") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "5"
+			} else if (Y == "2") {
+				 return "10"
+			} else if (Y == "3") {
+				 return "15"
+			} else if (Y == "4") {
+				 return "20"
+			} else if (Y == "5") {
+				 return "25"
+			} else if (Y == "6") {
+				 return "30"
+			}
+		} else if (X == "6") {
+			if (Y == "0") {
+				 return "0"
+			} else if (Y == "1") {
+				 return "6"
+			} else if (Y == "2") {
+				 return "12"
+			} else if (Y == "3") {
+				 return "18"
+			} else if (Y == "4") {
+				 return "24"
+			} else if (Y == "5") {
+				 return "30"
+			} else if (Y == "6") {
+				 return "36"
+			}
+		}
+	}
+}
+
+module.exports = {
+	Calculate: Calculate
+}
